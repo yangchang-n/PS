8cb00ebf6df7673942acf238c6c26a9abef95e8d
Yang Changeun <tomtomato@naver.com>
2023-12-31 19:40:23 +0900

    Create hello_002.cpp

---
+++ hello_002.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World" << endl;
+
+    char user_input[100];
+    cin >> user_input;
+    cout << user_input;
+
+    return 0;
+}
