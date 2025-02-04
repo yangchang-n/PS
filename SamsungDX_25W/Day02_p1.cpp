5f4b7ae7efc5871715e5340f4e999a4b823777e9
Yang Changeun <tomtomato@naver.com>
2025-02-04 12:11:22 +0900

    Update Day02_p1.cpp

--- SamsungDX_25W/Day02_p1.cpp
+++ SamsungDX_25W/Day02_p1.cpp
@@ -22,36 +22,36 @@ int main(int argc, char** argv)
     
 	for (int test_case = 1; test_case <= 10; ++test_case)
 	{
-	sequence[100] = {0};
-	seq_cnt = 0;
-	
-	int N;
-	cin >> N;
-	
-	char tree[N + 2];
-	in_order(1, N);
-	
-	for (int i = 0; i < N; ++i)
-	{
-	    int node_num, left, right;
-	    char value;
-	
-	    cin >> node_num >> value;
-	    if (cin.peek() != '\n') cin >> left;
-	    if (cin.peek() != '\n') cin >> right;
-	
-	    tree[node_num] = value;
+        sequence[100] = {0};
+        seq_cnt = 0;
+        
+        int N;
+        cin >> N;
+
+        char tree[N + 2];
+        in_order(1, N);
+
+        for (int i = 0; i < N; ++i)
+        {
+            int node_num, left, right;
+            char value;
+
+            cin >> node_num >> value;
+            if (cin.peek() != '\n') cin >> left;
+            if (cin.peek() != '\n') cin >> right;
+
+            tree[node_num] = value;
+        }
+
+        cout << '#' << test_case << ' ';
+
+        for (int i = 0; i < N; ++i)
+        {
+            cout << tree[sequence[i]];
+        }
+
+        cout << '\n';
 	}
-	
-	cout << '#' << test_case << ' ';
-	
-	for (int i = 0; i < N; ++i)
-	{
-	    cout << tree[sequence[i]];
-	}
-	
-	cout << '\n';
-	}
-	
+    
 	return 0;
 }
