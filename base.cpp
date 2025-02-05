e67f807936183f5791686b732d2b9c03fa827f22
Yang Changeun <tomtomato@naver.com>
2025-02-05 20:31:43 +0900

    Create base.cpp

---
+++ base.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+	int T;
+	cin>>T;
+
+	for(int test_case = 1; test_case <= T; ++test_case)
+	{
+        
+	}
+    
+	return 0;
+}
