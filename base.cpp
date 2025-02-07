20fb1e513e5e4d696e11c041d6742790c285a163
Yang Changeun <tomtomato@naver.com>
2025-02-07 20:55:41 +0900

    Update base.cpp

--- base.cpp
+++ base.cpp
@@ -4,14 +4,13 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-    ios_base::sync_with_stdio(false);
+    ios::sync_with_stdio(false);
     cin.tie(NULL);
-    cout.tie(NULL);
 
 	int T;
 	cin >> T;
 
-	for(int test_case = 1; test_case <= T; ++test_case)
+	for (int test_case = 1; test_case <= T; ++test_case)
 	{
         
 	}
