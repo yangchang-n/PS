29e0de5a387252fcb64d60d0cd1bbe2a142b87a2
Yang Changeun <tomtomato@naver.com>
2025-05-12 16:05:15 +0900

    Create 01402_S5_아무래도이문제는A번난이도인것같다.cpp

---
+++ acmicpc/01402_S5_아무래도이문제는A번난이도인것같다.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int N;
+    cin >> N;
+    
+    while (N--) cout << "yes\n";
+    
+    return 0;
+}
