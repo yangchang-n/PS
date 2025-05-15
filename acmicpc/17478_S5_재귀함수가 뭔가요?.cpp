7d7b0c04f118e38f25c1f5683527fadfa9fa261c
Yang Changeun <tomtomato@naver.com>
2025-05-15 14:13:25 +0900

    Update 17478_S5_재귀함수가 뭔가요?.cpp

--- acmicpc/17478_S5_재귀함수가 뭔가요?.cpp
+++ acmicpc/17478_S5_재귀함수가 뭔가요?.cpp
@@ -9,11 +9,14 @@ void cout_rec(int N, int s)
 
 	if (!s) cout << "어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n";
 	cout << underbars << "\"재귀함수가 뭔가요?\"\n";
-	if (N != s) cout << underbars << "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n";
-	if (N != s) cout << underbars << "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n";
-	if (N != s) cout << underbars << "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n";
-	if (N == s) cout << underbars << "\"재귀함수는 자기 자신을 호출하는 함수라네\"\n";
-	if (N != s) cout_rec(N, s + 1);
+	if (N != s)
+	{
+		cout << underbars << "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n";
+		cout << underbars << "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n";
+		cout << underbars << "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n";
+		cout_rec(N, s + 1);
+	}
+	else cout << underbars << "\"재귀함수는 자기 자신을 호출하는 함수라네\"\n";
 	cout << underbars << "라고 답변하였지.\n";
 }
 
