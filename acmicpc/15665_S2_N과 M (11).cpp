2795af0e0a3ae14a0964135a2526e1e0c2822969
Yang Changeun <tomtomato@naver.com>
2025-05-28 15:00:04 +0900

    Create 15665_S2_N과 M (11).cpp

---
+++ acmicpc/15665_S2_N과 M (11).cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+int num[10];
+int arr[10];
+
+void bkt(int p)
+{
+	if (p == M)
+	{
+		for (int i = 0; i < M; ++i) cout << num[arr[i]] << ' ';
+		cout << '\n';
+		return;
+	}
+
+	int pre = 0;
+	for (int i = 0; i < N; ++i)
+	{
+		if (num[i] == pre) continue;
+		pre = num[i];
+
+		arr[p] = i;
+		bkt(p + 1);
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	cin >> N >> M;
+	for (int i = 0; i < N; ++i) cin >> num[i];
+	sort(num, num + N);
+
+	bkt(0);
+
+	return 0;
+}
