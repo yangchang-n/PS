daf3d3e2082cb3b67ca8fd48dcf0bcd13f56aad4
Yang Changeun <tomtomato@naver.com>
2025-06-10 16:55:33 +0900

    Create 24420_D3_집합 비교.cpp

---
+++ SWEA/24420_D3_집합 비교.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int T;
+	cin >> T;
+
+	for (int tc = 1; tc <= T; ++tc)
+	{
+		int A, B, tmp;
+		bool valid = true;
+		unordered_set<int> us;
+		cin >> A >> B;
+
+		if (A == B)
+		{
+			for (int i = 0; i < A; ++i)
+			{
+				cin >> tmp;
+				us.insert(tmp);
+			}
+			for (int i = 0; i < B; ++i)
+			{
+				cin >> tmp;
+				if (!us.count(tmp)) valid = false;
+			}
+			
+			cout << (valid ? "=\n" : "?\n");
+		}
+		else if (A > B)
+		{
+			for (int i = 0; i < A; ++i)
+			{
+				cin >> tmp;
+				us.insert(tmp);
+			}
+			for (int i = 0; i < B; ++i)
+			{
+				cin >> tmp;
+				if (!us.count(tmp)) valid = false;
+			}
+
+			cout << (valid ? ">\n" : "?\n");
+		}
+		else
+		{
+			int can[50];
+			for (int i = 0; i < A; ++i)
+			{
+				cin >> tmp;
+				can[i] = tmp;
+			}
+			for (int i = 0; i < B; ++i)
+			{
+				cin >> tmp;
+				us.insert(tmp);
+			}
+			for (int i = 0; i < A; ++i)
+			{
+				if (!us.count(can[i])) valid = false;
+			}
+
+			cout << (valid ? "<\n" : "?\n");
+		}
+	}
+
+	return 0;
+}
