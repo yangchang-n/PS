35369bf681280e6c96d3485518a1a719d8d24d37
Yang Changeun <tomtomato@naver.com>
2025-07-17 17:31:37 +0900

    Update 01987_G4_알파벳.cpp

--- acmicpc/01987_G4_알파벳.cpp
+++ acmicpc/01987_G4_알파벳.cpp
@@ -5,12 +5,14 @@ using namespace std;
 const int di[4] = { 1, -1, 0, 0 };
 const int dj[4] = { 0, 0, 1, -1 };
 
-int R, C, ans = 1;
+int R, C, ans = 0;
 int bd[20][20];
 bool vs[26];
 
 void bkt(int i, int j, int cnt)
 {
+    ans = max(ans, cnt);
+    
 	for (int d = 0; d < 4; ++d)
 	{
 		int ni = i + di[d];
@@ -18,13 +20,8 @@ void bkt(int i, int j, int cnt)
 
 		if (ni < 0 || ni >= R) continue;
 		if (nj < 0 || nj >= C) continue;
-
-		if (vs[bd[ni][nj]])
-		{
-			ans = max(ans, cnt);
-			continue;
-		}
-
+		if (vs[bd[ni][nj]]) continue;
+        
 		vs[bd[ni][nj]] = true;
 		bkt(ni, nj, cnt + 1);
 		vs[bd[ni][nj]] = false;
